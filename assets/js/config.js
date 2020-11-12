83c88177aab0973d9a93d85dd0908daed203d5c0
Rob Skilling <rob@dxw.com>
2020-11-12 15:42:40 +0000

    Add GA script and basic branding
    Still to implement: passing GA ID to the script

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,43 @@
 var config = {
     apiKey: cookieControlConfig.apiKey,
     product: cookieControlConfig.productType,
-  };
+    closeStyle: 'button',
+    initialState: 'open',
+    text: {
+        closeLabel: 'Save and Close',
+        acceptSettings: 'Accept all cookies',
+        rejectSettings: 'Only accept necessary cookies'
+    },
+    branding: {
+        removeAbout: true
+    },
+    position: 'LEFT',
+    theme: 'DARK',
+    subDomains: false,
+    optionalCookies: [
+        {
+            name : 'analytics',
+            label: 'Analytical Cookies',
+            description: 'Analytical cookies help us to improve our website by collecting and reporting information on its usage.',
+            cookies: ['_ga', '_gid', '_gat', '__utma', '__utmt', '__utmb', '__utmc', '__utmz', '__utmv'],
+            onAccept : function(){
+                // Add Google Analytics
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+                ga('create', 'UA-XXXXX-Y', 'auto');
+                ga('send', 'pageview');
+                // End Google Analytics
+            },
+            onRevoke: function(){
+                // Disable Google Analytics
+                window['ga-disable-UA-XXXXX-Y'] = true;
+                // End Google Analytics
+            }
+        }
+    ]    
+};
 
 CookieControl.load(config);
