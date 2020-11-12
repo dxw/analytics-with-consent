0558a3995a984858a4f8a17cd0a0b504e83ad8af
Rob Skilling <rob@dxw.com>
2020-11-12 11:54:59 +0000

    Pass API Key & Product Type values to javascript

--- spec/scripts.spec.php
+++ spec/scripts.spec.php
@@ -37,16 +37,21 @@ describe(Scripts::class, function () {
                 });
             });
             context('and Civic Product Type is set', function () {
-                it('enqueues the Civic Cookie Control script and the config script', function () {
+                it('enqueues the Civic Cookie Control script and the config script, and injects our settings', function () {
                     allow('get_field')->toBeCalled()->andReturn('an_api_key', 'a_product_type');
                     expect('get_field')->toBeCalled()->once()->with('civic_cookie_control_api_key', 'option');
                     expect('get_field')->toBeCalled()->once()->with('civic_cookie_control_product_type', 'option');
                     allow('wp_enqueue_script')->toBeCalled();
                     expect('wp_enqueue_script')->toBeCalled()->once()->with('civicCookieControl', 'https://cc.cdn.civiccomputing.com/9/cookieControl-9.x.min.js');
                     allow('dirname')->toBeCalled()->andReturn('/path/to/this/plugin');
-                    allow('plugins_url')->toBeCalled()->andReturn('http://path/to/this/plugin/static/js/config.js');
-                    expect('plugins_url')->toBeCalled()->once()->with('/static/js/config.js', '/path/to/this/plugin');
-                    expect('wp_enqueue_script')->toBeCalled()->once()->with('civicCookieControlConfig', 'http://path/to/this/plugin/static/js/config.js', ['civicCookieControl']);
+                    allow('plugins_url')->toBeCalled()->andReturn('http://path/to/this/plugin/assets/js/config.js');
+                    expect('plugins_url')->toBeCalled()->once()->with('/assets/js/config.js', '/path/to/this/plugin');
+                    expect('wp_enqueue_script')->toBeCalled()->once()->with('civicCookieControlConfig', 'http://path/to/this/plugin/assets/js/config.js', ['civicCookieControl']);
+                    allow('wp_localize_script')->toBeCalled();
+                    expect('wp_localize_script')->toBeCalled()->once()->with('civicCookieControlConfig', 'cookieControlConfig', [
+                        'apiKey' => 'an_api_key',
+                        'productType' => 'a_product_type'
+                    ]);
                     $this->scripts->enqueueScripts();
                 });
             });
--- src/Scripts.php
+++ src/Scripts.php
@@ -11,9 +11,15 @@ class Scripts implements \Dxw\Iguana\Registerable
 
     public function enqueueScripts() : void
     {
-        if (get_field('civic_cookie_control_api_key', 'option') && get_field('civic_cookie_control_product_type', 'option')) {
+        $apiKey = get_field('civic_cookie_control_api_key', 'option');
+        $productType = get_field('civic_cookie_control_product_type', 'option');
+        if ($apiKey && $productType) {
             wp_enqueue_script('civicCookieControl', 'https://cc.cdn.civiccomputing.com/9/cookieControl-9.x.min.js');
-            wp_enqueue_script('civicCookieControlConfig', plugins_url('/static/js/config.js', dirname(__FILE__)), ['civicCookieControl']);
+            wp_enqueue_script('civicCookieControlConfig', plugins_url('/assets/js/config.js', dirname(__FILE__)), ['civicCookieControl']);
+            wp_localize_script('civicCookieControlConfig', 'cookieControlConfig', [
+                'apiKey' => $apiKey,
+                'productType' => $productType
+            ]);
         }
     }
 }
