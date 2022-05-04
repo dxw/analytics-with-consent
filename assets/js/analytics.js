ca10d495e94141158f2936d865d17624002f2726
Rob Skilling <rob@dxw.com>
2022-05-04 10:27:17 +0100

    Only run UA code if a UA ID is set
    This will ensure that if somebody is only using GA4, the plugin doesn't
    try and run UA as well.

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -2,22 +2,26 @@
 /* eslint-disable */
 var analyticsWithConsent = {
   gaAccept: function () {
-    // Add Google Analytics   
-    (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date(); a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
-  
-    ga('create', cookieControlDefaultAnalytics.googleAnalyticsId, 'auto')
-    ga('send', 'pageview')
-    // End Google Analytics   
+    // Add Google Analytics (UA)
+    if (cookieControlDefaultAnalytics.googleAnalyticsId !== '') {
+      (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+    
+      ga('create', cookieControlDefaultAnalytics.googleAnalyticsId, 'auto')
+      ga('send', 'pageview')        
+    }   
+    // End Google Analytics (UA)
   },
   gaRevoke: function () {
-    // Disable Google Analytics
-    window['ga-disable-' + cookieControlDefaultAnalytics.googleAnalyticsId] = true
-    // End Google Analytics
+    // Disable Google Analytics (UA)
+    if (cookieControlDefaultAnalytics.googleAnalyticsId !== '') {
+      window['ga-disable-' + cookieControlDefaultAnalytics.googleAnalyticsId] = true
+    }
+    // End Google Analytics (UA)
   }
 }
 /* eslint-enable */
