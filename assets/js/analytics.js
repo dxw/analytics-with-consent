999cb9412184f0715fa9b3e9722cd22b2b1adb7b
Brent <brent@dxw.com>
2022-09-13 12:27:43 +0100

    Add GTM script

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -22,6 +22,14 @@ var analyticsWithConsent = {
       });
     }
     // End GA4
+
+    // GTM
+    if (cookieControlDefaultAnalytics.gtmId !== '') {
+        window.dataLayer = window.dataLayer || [];
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer',cookieControlDefaultAnalytics.gtmId);
+    }
+    // End GTM
   },
   gaRevoke: function () {
     // Disable Google Analytics (UA)
