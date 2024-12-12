11876025c194ccef02d4de8cc95ccf6a9ca61201
Sim Brody <sim@dxw.com>
2024-12-12 10:26:01 +0000

    Check variable is not empty before attempting trim, to prevent PHP warning.

--- src/Scripts.php
+++ src/Scripts.php
@@ -22,12 +22,12 @@ class Scripts implements \Dxw\Iguana\Registerable
 
 	public function enqueueScripts(): void
 	{
-		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option'));
-		$productType = trim(get_field('civic_cookie_control_product_type', 'option'));
-		$googleAnalyticsId = trim(get_field('google_analytics_id', 'option'));
-		$ga4Id = trim(get_field('ga_4_id', 'option'));
-		$gtmId = trim(get_field('google_analytics_gtm', 'option'));
-		$hotjarId = trim(get_field('hotjar_id', 'option'));
+		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option') ?? '');
+		$productType = trim(get_field('civic_cookie_control_product_type', 'option') ?? '');
+		$googleAnalyticsId = trim(get_field('google_analytics_id', 'option') ?? '');
+		$ga4Id = trim(get_field('ga_4_id', 'option') ?? '');
+		$gtmId = trim(get_field('google_analytics_gtm', 'option') ?? '');
+		$hotjarId = trim(get_field('hotjar_id', 'option') ?? '');
 		if ($apiKey && $productType) {
 			wp_enqueue_script('civicCookieControl', 'https://cc.cdn.civiccomputing.com/9/cookieControl-9.x.min.js');
 			wp_enqueue_script('civicCookieControlDefaultAnalytics', plugins_url('/assets/js/analytics.js', dirname(__FILE__)), ['civicCookieControl']);
@@ -49,9 +49,9 @@ class Scripts implements \Dxw\Iguana\Registerable
 
 	public function addGA4(): void
 	{
-		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option'));
-		$productType = trim(get_field('civic_cookie_control_product_type', 'option'));
-		$ga4Id = trim(get_field('ga_4_id', 'option'));
+		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option') ?? '');
+		$productType = trim(get_field('civic_cookie_control_product_type', 'option') ?? '');
+		$ga4Id = trim(get_field('ga_4_id', 'option') ?? '');
 		if ($apiKey && $productType && $ga4Id) {
 			printf('<script async id="awc_gtag" src="https://www.googletagmanager.com/gtag/js?id=%s"></script>', esc_attr($ga4Id));
 		}
@@ -59,9 +59,9 @@ class Scripts implements \Dxw\Iguana\Registerable
 
 	public function addGTM(): void
 	{
-		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option'));
-		$productType = trim(get_field('civic_cookie_control_product_type', 'option'));
-		$gtmId = trim(get_field('google_analytics_gtm', 'option'));
+		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option') ?? '');
+		$productType = trim(get_field('civic_cookie_control_product_type', 'option') ?? '');
+		$gtmId = trim(get_field('google_analytics_gtm', 'option') ?? '');
 		if ($apiKey && $productType && $gtmId) {
 			printf("<script>window.dataLayer = window.dataLayer || []; (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','%s')</script>", esc_js($gtmId));
 		}
@@ -90,8 +90,8 @@ class Scripts implements \Dxw\Iguana\Registerable
 			];
 		}
 		return apply_filters('awc_civic_cookie_control_config', [
-			'apiKey' => trim(get_field('civic_cookie_control_api_key', 'option')),
-			'product' => trim(get_field('civic_cookie_control_product_type', 'option')),
+			'apiKey' => trim(get_field('civic_cookie_control_api_key', 'option') ?? ''),
+			'product' => trim(get_field('civic_cookie_control_product_type', 'option') ?? ''),
 			'closeStyle' => 'button',
 			'initialState' => 'open',
 			'text' => [
