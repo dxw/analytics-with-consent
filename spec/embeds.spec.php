be663c2d1a993045722c89178c17cb785ebe1270
Rob Skilling <rob@dxw.com>
2026-05-29 11:16:29 +0100

    Filter embed blocks and classic editor embeds separately
    Although we'd like the final output to look roughly the same, it's
    useful if we can handle block embeds and classic editor embeds
    separately, as block embeds aren't guaranteed to trigger the
    `embed_oembed_html` filter, whereas they should always trigger the
    `render_block` filter.
    
    Therefore, let's use `render_block` where we can, and fall back to
    `embed_oembed_html` if `render_block` hasn't been triggered (e.g. for
    classic editor embeds).

--- spec/embeds.spec.php
+++ spec/embeds.spec.php
@@ -15,8 +15,47 @@ describe(Embeds::class, function () {
 		it('adds the embed filter', function () {
 			allow('add_filter')->toBeCalled();
 			expect('add_filter')->toBeCalled()->once()->with('embed_oembed_html', [$this->embeds, 'embedPlaceholder'], 10, 4);
+			expect('add_filter')->toBeCalled()->once()->with('render_block', [$this->embeds, 'filterBlock']);
 
 			$this->embeds->register();
+		});
+	});
+
+	describe('->filterBlock()', function () {
+		context('ACF is disabled', function () {
+			it('returns the block content', function () {
+				allow('function_exists')->toBeCalled()->andReturn(false);
+
+				expect($this->embeds->filterBlock('<iframe>embed</iframe>', ['blockName' => 'core/embed']))->toEqual('<iframe>embed</iframe>');
+			});
+		});
+		context('third party media consent is disabled', function () {
+			it('returns the block content', function () {
+				allow('function_exists')->toBeCalled()->andReturn(true);
+				allow('get_field')->toBeCalled()->andReturn(false);
+
+				expect($this->embeds->filterBlock('<iframe>embed</iframe>', ['blockName' => 'core/embed']))->toEqual('<iframe>embed</iframe>');
+			});
+		});
+		context('third party media consent is enabled', function () {
+			context('but we are in the admin panel', function () {
+				it('returns the block content', function () {
+					allow('function_exists')->toBeCalled()->andReturn(true);
+					allow('get_field')->toBeCalled()->andReturn(true);
+					allow('is_admin')->toBeCalled()->andReturn(true);
+
+					expect($this->embeds->filterBlock('<iframe>embed</iframe>', ['blockName' => 'core/embed']))->toEqual('<iframe>embed</iframe>');
+				});
+			});
+			context('and we are in the front end', function () {
+				it('returns the placeholder', function () {
+					allow('function_exists')->toBeCalled()->andReturn(true);
+					allow('get_field')->toBeCalled()->andReturn(true);
+					allow('is_admin')->toBeCalled()->andReturn(false);
+
+					expect($this->embeds->filterBlock('<iframe>embed</iframe>', ['blockName' => 'core/embed']))->toContain('awc-embed-placeholder');
+				});
+			});
 		});
 	});
 
@@ -45,6 +84,17 @@ describe(Embeds::class, function () {
 		});
 
 		context('third party embed consent setting is enabled and not within the admin panel', function () {
+			context('but the placeholder is already in place', function () {
+				it('returns the input', function () {
+					allow('function_exists')->toBeCalled()->andReturn(true);
+					allow('get_field')->toBeCalled()->with('third_party_media_embed_consent', 'option')->andReturn(true);
+					allow('is_admin')->toBeCalled()->andReturn(false);
+
+					$result = $this->embeds->embedPlaceholder('<div class="awc-embed-placeholder">Hello</div>', 'https://example.com/video', [], 123);
+
+					expect($result)->toEqual('<div class="awc-embed-placeholder">Hello</div>');
+				});
+			});
 			it('returns the placeholder html', function () {
 				allow('function_exists')->toBeCalled()->andReturn(true);
 				allow('get_field')->toBeCalled()->with('third_party_media_embed_consent', 'option')->andReturn(true);
--- src/Embeds.php
+++ src/Embeds.php
@@ -7,11 +7,26 @@ class Embeds implements \Dxw\Iguana\Registerable
 	public function register(): void
 	{
 		add_filter('embed_oembed_html', [$this, 'embedPlaceholder'], 10, 4);
+		add_filter('render_block', [$this, 'filterBlock'], 10, 2);
+	}
+
+	public function filterBlock(string $blockContent, array $block): string
+	{
+		if (!$this->isThirdPartyMediaEmbedConsentEnabled() || is_admin()) {
+			return $blockContent;
+		}
+		if (strpos($block['blockName'], 'core-embed/') === 0 || $block['blockName'] === 'core/embed') {
+			return $this->placeholderMarkup($blockContent);
+		}
+		return $blockContent;
 	}
 
 	public function embedPlaceholder(string $html, string $url, array $attr, int $post_ID): string
 	{
 		if (!$this->isThirdPartyMediaEmbedConsentEnabled() || is_admin()) {
+			return $html;
+		}
+		if (str_contains($html, 'awc-embed-placeholder')) {
 			return $html;
 		}
 		return $this->placeholderMarkup($html);
