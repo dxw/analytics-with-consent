ec83d106024c023c4771fdf0c541b7f88ccb2cca
Matthew Passmore <matthew@dxw.com>
2026-06-30 09:33:24 +0100

    Update unit tests

--- src/Scripts.php
+++ src/Scripts.php
@@ -60,8 +60,13 @@ class Scripts implements \Dxw\Iguana\Registerable
 		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option') ?? '');
 		$productType = trim(get_field('civic_cookie_control_product_type', 'option') ?? '');
 		$ga4Id = trim(get_field('ga_4_id', 'option') ?? '');
+
 		if ($apiKey && $productType && $ga4Id) {
-			wp_print_script_tag(['async' => true, 'id' => 'awc_gtag', 'src' => esc_url("https://www.googletagmanager.com/gtag/js?id=".$ga4Id)]);
+			wp_print_script_tag([
+				'async' => true,
+				'id' => 'awc_gtag',
+				'src' => esc_url('https://www.googletagmanager.com/gtag/js?id=' . $ga4Id),
+			]);
 		}
 	}
 
@@ -74,12 +79,14 @@ class Scripts implements \Dxw\Iguana\Registerable
 		$apiKey = trim(get_field('civic_cookie_control_api_key', 'option') ?? '');
 		$productType = trim(get_field('civic_cookie_control_product_type', 'option') ?? '');
 		$gtmId = trim(get_field('google_analytics_gtm', 'option') ?? '');
-		if ($apiKey && $productType && $gtmId) {
-			$js_tracking_id = "
-window.dataLayer = window.dataLayer || [];
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','".esc_js($gtmId)."');";
-			wp_print_inline_script_tag($js_tracking_id);
+		if ($apiKey && $productType && $gtmId) {
+			$gtmTrackingScript = "
+				window.dataLayer = window.dataLayer || [];
+
+				(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','".esc_js($gtmId)."');";
+
+			wp_print_inline_script_tag($gtmTrackingScript);
 		}
 	}
 
